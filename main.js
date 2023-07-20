a8db2127ee7fec5c584fca2b59b7f6f04351088b
Hasul79 <hatuk2@mail.ru>
2023-07-21 00:52:34 +0400

    create bulb

---
+++ main.js
@@ -0,0 +1,7 @@
+// let name = prompt('Ինչ է քո անունը');
+// let age = prompt("Քանի տարեկան ես դու?");
+// alert("Բարև " + age + " տարեկան " +  name);
+
+ document.getElementById("demo").innerHTML="JavaScript";
+
+ 
